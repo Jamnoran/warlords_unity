b173d090c23c53fb0ba5eba01b73922c8ff95698
darrrrUC <ema_carlsson@yahoo.com>
2016-06-28 20:41:18 +0200

    baseclasses for warrior and priest

---
+++ warlords/Assets/scripts/character classes/baseWarrior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class baseWarrior : baseCharacter {
+
+    //create out basic warrior class, derived from baseCharacter class.
+    public baseWarrior()
+    {
+        CharacterClassName        = "Warrior";
+        CharacterClassDescription = "Forged from the fires of sala'bakke, the warrior is pround and sturdy.";
+        Stamina                   = 100;
+        Endurance                 = 10;
+        Strength                  = 25;
+        Intellect                 = 2;
+        Agility                   = 4;
+        Armor                     = 40;
+    }
+	
+}
---
+++ warlords/Assets/scripts/testing/TestGUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestGUI : MonoBehaviour {
+
+    private baseCharacter class1 = new basePriest();
+    private baseCharacter class2 = new baseWarrior();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
