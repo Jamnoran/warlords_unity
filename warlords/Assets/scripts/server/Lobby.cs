53a1645d84ea9a67e7a427f2cb0854aa37a50ea2
jamnoran <lindahl.eric@gmail.com>
2016-08-01 20:10:43 +0200

    Lobby

---
+++ warlords/Assets/scripts/server/Lobby.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Assets.scripts.vo;
+
+public class Lobby : MonoBehaviour {
+    public Button changeHero = null;
+    public Button quickGame = null;
+    public Button topLeftButton = null;
+    public Button topRightButton = null;
+    public Button botLeftButton = null;
+    public Button botRightButton = null;
+
+    private Hero currentHero = null;
+
+    // Use this for initialization
+    void Start () {
+        changeHero.onClick.AddListener(() => { showheroDialog(); });
+        quickGame.onClick.AddListener(() => { startQuickGame(); });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void showheroDialog() {
+        Debug.Log("Showing hero dialog");
+    }
+
+    void startQuickGame()
+    {
+        Debug.Log("Starting a quick game");
+    }
+}
