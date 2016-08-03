8b67152787bb70243ac09c8b82e752ea0377d1ec
darrrrUC <ema_carlsson@yahoo.com>
2016-08-03 21:29:49 +0200

    healthbar updating

--- warlords/Assets/scripts/ui/Player.cs
+++ warlords/Assets/scripts/ui/Player.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    [SerializeField]
+    private Stat health;
+
+	private void Awake()
+    {
+        health.Initialize();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
-	}
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            health.CurrentVal -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            health.CurrentVal += 10;
+        }
+    }
 }
--- warlords/Assets/scripts/ui/Stat.cs
+++ warlords/Assets/scripts/ui/Stat.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class Stat : MonoBehaviour {
+[Serializable]
+public class Stat  {
+    [SerializeField]
+    private BarScript bar;
+    [SerializeField]
+    private float maxVal;
+    [SerializeField]
+    private float currentVal;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    //update current health
+    public float CurrentVal 
+    {
+        get
+        {
+            return currentVal;
+        }
+
+        set
+        {
+            this.currentVal = value;
+            bar.Value = currentVal;
+        }
+    }
+
+
+    //update max health
+    public float MaxVal
+    {
+        get
+        {
+            return maxVal;
+        }
+
+        set
+        {
+            this.maxVal = value;
+            bar.MaxValue = maxVal;
+        }
+    }
+
+    public void Initialize()
+    {
+        this.MaxVal = maxVal;
+        this.CurrentVal = currentVal;
+    }
 }
