4e0f945e0e910260a76140a5d60dc41e5549013c
ErCa <lindahl.eric@gmail.com>
2016-09-30 21:11:21 +0200

    Add functionality for drag and drop

---
+++ warlords/Assets/scripts/ui/Draggable.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    #region private varialbes
+    private Vector2 startPosition;
+    #endregion
+
+    #region public variables
+    public Transform parentToReturnTo = null;
+    public enum Slot { WEAPON, HEAD, CHEST, LEGS, FEET};
+    public Slot typeOfItem = Slot.WEAPON;
+    #endregion
+
+
+    public void Start()
+    {
+        startPosition = this.transform.position;
+
+    }
+
+    /// <summary>
+    /// Handle events when user beings to drag an UI item
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Started draging object");
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    /// <summary>
+    /// Handles the current draging of an UI item
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Currently draging object");
+        this.transform.position = eventData.position;
+        
+    }
+    /// <summary>
+    /// We end up here when we have finished draging an object
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //this.transform.position = startPosition;
+        Debug.Log("Stoped draging object");
+        this.transform.SetParent(parentToReturnTo);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
---
+++ warlords/Assets/scripts/ui/DropZone.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+
+    #region public variables
+    public Draggable.Slot typeOfItem = Draggable.Slot.WEAPON;
+    #endregion
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerEnter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerExit");
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop to: " + gameObject.name);
+        Draggable itemToDrag = eventData.pointerDrag.GetComponent<Draggable>();
+        if (itemToDrag != null)
+        {
+            if(typeOfItem == itemToDrag.typeOfItem)
+            { 
+            itemToDrag.parentToReturnTo = this.transform;
+            }
+        }
+    }
+}
