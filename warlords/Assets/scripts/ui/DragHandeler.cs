452d98f093449a188336a674d0c2dd5c330c0d8d
darrrrUC <ema_carlsson@yahoo.com>
2016-11-05 15:56:01 +0100

    foo

---
+++ warlords/Assets/scripts/ui/DragHandeler.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragHandeler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Transform SetThisAsParent;
+    public static GameObject itemBeingDragged;
+    Vector3 startPosition;
+    Transform startParent;
+
+
+    #region IBeginDragHandler implementation
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+        itemBeingDragged = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        GameObject myObject = (GameObject)Instantiate(this.gameObject, new Vector3(0,0,0), Quaternion.identity);
+        myObject.transform.SetParent(SetThisAsParent);
+        
+
+    }
+
+    #endregion
+
+    #region IDragHandler implementation
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    #endregion
+
+    #region IEndDragHandler implementation
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        itemBeingDragged = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent == startParent)
+        {
+            transform.position = startPosition;
+        }
+    }
+
+    #endregion
+
+
+
+}
