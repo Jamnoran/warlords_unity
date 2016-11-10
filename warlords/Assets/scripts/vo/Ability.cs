d6ab404200213cbd81d92f93f5b345e2885e40b1
jamnoran <lindahl.eric@gmail.com>
2016-11-10 14:58:29 +0100

    Get abilities from server

---
+++ warlords/Assets/scripts/vo/Ability.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.scripts.vo
+{
+    public class Ability
+    {
+        public int id;
+        public String classType;
+        public String name;
+        public int levelReq;
+        public String damageType;
+        public String description;
+        public String image;
+        public int baseDamage;
+        public int crittable;
+        public String targetType;
+        public int baseCD;
+    }
+}
---
+++ warlords/Assets/scripts/vo/ResponseAbilities.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.scripts.vo;
+
+[System.Serializable]
+public class ResponseAbilities {
+	public string response_type = "ABILITIES";
+    public List<Ability> abilities;
+
+    public ResponseAbilities(){}
+
+
+}
