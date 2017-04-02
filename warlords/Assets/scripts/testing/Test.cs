07ce04f25580549087b00975eadb08d344540c03
ErCa <lindahl.eric@gmail.com>
2017-04-02 22:19:49 +0200

    Fixes for movement and multiple clients

---
+++ warlords/Assets/scripts/testing/Test.cs
@@ -0,0 +1,36 @@
+﻿using Assets.scripts.vo;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    public Transform minion1;
+
+    // Use this for initialization
+    void Start () {
+        initiateMinon();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyUp("c")) {
+            initiateMinon();
+        }
+    }
+
+
+    void initiateMinon() {
+        Minion newMinion = new Minion();
+        newMinion.desiredPositionX = -1.5f;
+        newMinion.desiredPositionZ = 3.5f;
+
+        Transform minionTransform = (Transform)Instantiate(minion1, new Vector3(newMinion.desiredPositionX, 1.0f, newMinion.desiredPositionZ), Quaternion.identity);
+        newMinion.setTransform(minionTransform);
+        newMinion.initBars();
+        MinionAnimations minionAnimations = (MinionAnimations)minionTransform.GetComponent(typeof(MinionAnimations));
+        minionAnimations.setDesiredLocation(new Vector3(newMinion.desiredPositionX, 1.0f, newMinion.desiredPositionZ));
+        FieldOfView fieldOfView = ((FieldOfView)minionTransform.Find("mob1").GetComponent(typeof(FieldOfView)));
+    }
+}
