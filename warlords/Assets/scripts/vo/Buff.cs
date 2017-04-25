e0c879206463c5e0d9fef7a8c07d8a63e8929768
ErCa <lindahl.eric@gmail.com>
2017-04-25 21:13:06 +0200

    Fixed bugg with movement for multiplayer

---
+++ warlords/Assets/scripts/vo/Buff.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.scripts.vo {
+    public class Buff {
+        public static int SPEED = 1;
+	    public static int SHIELD = 2;
+    }
+}
