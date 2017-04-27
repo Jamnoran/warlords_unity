cfab2ab812eaa24214f74bc30f67cb84286f5573
ErCa <lindahl.eric@gmail.com>
2017-04-27 17:37:37 +0200

    First scene with dungen

---
+++ warlords/Assets/scripts/game/SpawnPointLogic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointLogic : MonoBehaviour {
+
+    public bool start = false;
+    public GameObject point1;
+    public GameObject point2;
+    public GameObject point3;
+    public GameObject point4;
+
+    // Use this for initialization
+    void Start () {
+        getTestSpawn().registerSpawnPoint(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    TestSpawn getTestSpawn() {
+        return ((TestSpawn)GameObject.Find("TestObject").GetComponent(typeof(TestSpawn)));
+    }
+}
---
+++ warlords/Assets/scripts/game/TestSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSpawn : MonoBehaviour {
+
+    public bool foundSpawnPoint = false;
+    public List<GameObject> spawns = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void registerSpawnPoint(GameObject spawnPoint) {
+        if (spawns.Count == 0) {
+            ((SpawnPointLogic)spawnPoint.GetComponent(typeof(SpawnPointLogic))).start = true;
+            Debug.Log("Send to server we got spawn point");
+        }
+        spawns.Add(spawnPoint);
+    }
+
+}
