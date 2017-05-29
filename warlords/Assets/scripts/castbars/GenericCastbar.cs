863f2d59be0f03dfc7dfba2f62696f6ce2cd6cb1
darrrrUC <ema_carlsson@yahoo.com>
2017-05-29 13:40:46 +0200

    icons updated and actionbar hotkeys now in front of icons

--- warlords/Assets/scripts/castbars/GenericCastbar.cs
+++ warlords/Assets/scripts/castbars/GenericCastbar.cs
@@ -6,6 +6,14 @@ using UnityEngine.UI;
 
 public class GenericCastbar : MonoBehaviour {
 
+    private GameObject spell1;
+    private GameObject spell2;
+    private GameObject spell3;
+    private GameObject spell4;
+    private GameObject spell5;
+    private GameObject spell6;
+    private GameObject spell7;
+
     public Image castBarFiller;
     public GameObject textToUpdate;
     private Text tmpTxt;
@@ -15,6 +23,13 @@ public class GenericCastbar : MonoBehaviour {
 
     private void Start()
     {
+        spell1 = GameObject.FindGameObjectWithTag("spell1");
+        spell2 = GameObject.FindGameObjectWithTag("spell2");
+        spell3 = GameObject.FindGameObjectWithTag("spell3");
+        spell4 = GameObject.FindGameObjectWithTag("spell4");
+        spell5 = GameObject.FindGameObjectWithTag("spell5");
+        spell6 = GameObject.FindGameObjectWithTag("spell6");
+        spell7 = GameObject.FindGameObjectWithTag("spell7");
 
         tmpTxt = textToUpdate.GetComponent<Text>();
         //instantiate casting to false, just in case something wierd sets it to true
@@ -30,6 +45,35 @@ public class GenericCastbar : MonoBehaviour {
 
     void Update () {
 
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            FindSpellAux(spell1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            FindSpellAux(spell2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            FindSpellAux(spell3);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            FindSpellAux(spell4);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            FindSpellAux(spell5);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            FindSpellAux(spell6);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            FindSpellAux(spell7);
+        }
         //temp key so we can test this 
         if (Input.GetKeyDown("h"))
         {
@@ -54,5 +98,27 @@ public class GenericCastbar : MonoBehaviour {
         isCasting = false;
         castBarFiller.fillAmount = 0;
         timeLeft = castTime;
+    }
+
+    private void FindSpellAux(GameObject spell)
+    {
+
+        if (spell.transform.childCount > 0)
+        {
+            List<int> enemies = new List<int>();
+            enemies.Add(getGameLogic().getMyHero().targetEnemy);
+            List<int> friends = new List<int>();
+            friends.Add(getGameLogic().getMyHero().targetFriendly);
+            getGameLogic().sendSpell((int)getGameLogic().getAbilityIdByAbilityName(spell.transform.GetChild(0).GetComponent<Image>().sprite.name), enemies, friends);
+        }
+        else
+        {
+            Debug.Log("No spell assigned to ");
+        }
+    }
+
+    GameLogic getGameLogic()
+    {
+        return ((GameLogic)GameObject.Find("GameLogicObject").GetComponent(typeof(GameLogic)));
     }
 }
