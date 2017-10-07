8a6c8d3289ca41a7d32ef6e91fe2b96d5bb0e55e
ErCa <lindahl.eric@gmail.com>
2017-10-07 16:31:34 +0200

    New warlock

---
+++ warlords/Assets/Scripts/game/HideObjectsOnRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideObjectsOnRotation : MonoBehaviour {
+
+    public GameObject parentToCheckObjective;
+    public List<int> rotationsToHide = new List<int>();
+    public List<GameObject> objectsToHide = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+        if (parentToCheckObjective != null)
+        {
+            foreach (GameObject objectToHide in objectsToHide)
+            {
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
