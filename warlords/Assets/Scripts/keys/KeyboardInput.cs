fc9bb1625bb88d47f026c1b226b6af67e08d239c
ErCa <lindahl.eric@gmail.com>
2017-11-19 23:36:17 +0100

    Started work on keyboard input

---
+++ warlords/Assets/Scripts/keys/KeyboardInput.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInput : MonoBehaviour {
+
+    public int state = 0;
+
+    public static int READY = 0;
+    public static int LOBBY = 1;
+    public static int IN_GAME = 2;
+    public static int CHAT = 3;
+    public static int MENU = 4;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown("escape"))
+        {
+
+        }
+        if (Input.GetKeyUp("enter"))
+        { 
+
+        }
+        // Spell input
+
+        // Chat input
+
+        // Menu input
+
+        // Character input
+
+        // Talent input
+
+        if (Input.GetKeyDown("t"))
+        {
+
+        }
+        // Targeting system
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            getTargetingByFKeys().setTarget(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.F2))
+        {
+            getTargetingByFKeys().setTarget(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.F3))
+        {
+            getTargetingByFKeys().setTarget(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.F4))
+        {
+            getTargetingByFKeys().setTarget(3);
+        }
+    }
+
+
+
+
+    TargetingByFKeys getTargetingByFKeys()
+    {
+        return ((TargetingByFKeys)GameObject.Find("GameLogicObject").GetComponent(typeof(TargetingByFKeys)));
+    }
+    
+}
