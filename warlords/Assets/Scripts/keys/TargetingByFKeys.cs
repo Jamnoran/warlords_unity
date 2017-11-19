d617a2dcf7ffcd7f230fdec1553ef9c5e35458d5
ErCa <lindahl.eric@gmail.com>
2017-11-19 23:36:01 +0100

    Started work on keyboard input

--- warlords/Assets/Scripts/keys/TargetingByFKeys.cs
+++ warlords/Assets/Scripts/keys/TargetingByFKeys.cs
@@ -3,36 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TargetingByFKeys : MonoBehaviour {
+public class TargetingByFKeys : MonoBehaviour{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.F1))
-        {
-            setTarget(0);
-        }
-        else if (Input.GetKeyDown(KeyCode.F2))
-        {
-            setTarget(1);
-        }
-        else if (Input.GetKeyDown(KeyCode.F3))
-        {
-            setTarget(2);
-        }
-        else if (Input.GetKeyDown(KeyCode.F4))
-        {
-            setTarget(3);
-        }
-
+    // Use this for initialization
+    void Start()
+    {
 
     }
 
-    private void setTarget(int position)
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+        public void setTarget(int position)
     {
         Hero myHero = getGameLogic().getMyHero();
         if (getGameLogic().getHeroes().Count > position)
