f3d268b9f8735be7dc4b337f6fa66c055e659bc2
ErCa <lindahl.eric@gmail.com>
2017-12-17 21:45:14 +0100

    Gauntlet mode

---
+++ warlords/Assets/Scripts/game/GauntletMode.cs
@@ -0,0 +1,52 @@
+﻿using Assets.scripts.util;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GauntletMode : MonoBehaviour {
+
+	public bool currentMode = false;
+    public long startTime = 0;
+    public long timeLimit = 30000;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(currentMode && !enabled){
+            if (DeviceUtil.getMillis() >= (startTime + timeLimit))
+            {
+                // Team lost, show failed message
+                getNotificationhandler().showNotification(1, "You lost, restart to continue!");
+            }
+            else
+            {
+                getNotificationhandler().showNotification(1, "Time left : " + getPrettyTime((startTime + timeLimit) - DeviceUtil.getMillis()));
+            }
+		}
+	}
+
+    private long getPrettyTime(long time)
+    {
+        return time / 1000;
+    }
+
+    public void startTimer()
+    {
+        startTime = DeviceUtil.getMillis();
+    }
+    
+
+    GameLogic getGameLogic() {
+		return ((GameLogic)GameObject.Find("GameLogicObject").GetComponent(typeof(GameLogic)));
+	}
+
+
+    NotificationHandler getNotificationhandler()
+    {
+        return ((NotificationHandler)GameObject.Find("GameLogicObject").GetComponent(typeof(NotificationHandler)));
+    }
+}
---
+++ warlords/Assets/Scripts/game/LoadingInGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingInGame : MonoBehaviour {
+
+    public GameObject loadingOverlay;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void showLoading()
+    {
+        loadingOverlay.SetActive(true);
+    }
+
+    public void hideLoading()
+    {
+        loadingOverlay.SetActive(false);
+    }
+
+    public void setLoadingText(string loadingText)
+    {
+
+    }
+}
